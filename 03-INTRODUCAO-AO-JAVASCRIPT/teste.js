ab95da3b69e97fdf0f22e1a79d30395fd5bd4d2a
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-25 23:36:10 -0300

    Funções

--- 03-INTRODUCAO-AO-JAVASCRIPT/teste.js
+++ 03-INTRODUCAO-AO-JAVASCRIPT/teste.js
@@ -1,10 +1,21 @@
+//Variaveis
+
 /*var y = 0;
 var x = 1;*/
 
 //Função do 1º Grau | ax + b = 0
 
-var preco = 2;
+/*var preco = 2;
 var desconto = 0.2;
 
-proco = preco - desconto;
+proco = preco - desconto;*/
 
+//Funções
+
+function soma(a,b)
+{
+    console.log(a + b);
+    return a + b;
+}
+
+soma(5 + 2);
