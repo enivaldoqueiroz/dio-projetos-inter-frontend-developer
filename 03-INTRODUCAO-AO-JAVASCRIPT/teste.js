f56274c1ba320298e1d2cdf0ab783c2962e90371
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-25 23:54:08 -0300

    Utilizando o console.log() com nodejs

--- 03-INTRODUCAO-AO-JAVASCRIPT/teste.js
+++ 03-INTRODUCAO-AO-JAVASCRIPT/teste.js
@@ -12,10 +12,33 @@ proco = preco - desconto;*/
 
 //Funções
 
-function soma(a,b)
+/*function soma(a,b)
 {
     console.log(a + b);
     return a + b;
 }
 
-soma(5 + 2);
+soma(5 + 2);*/
+
+//console.log("Hello World!");
+
+function returnEvenValues(array)
+{
+    let evenNums = [];
+    for(let i = 0; i < array.length; i++)
+    {
+        if(array[i] % 2 === 0)
+        {
+            evenNums.push(array[i]);
+        } 
+        else 
+        {
+            console.log(`${array[i]} não é par!`);
+        }
+    }
+    console.log('Os numeros pares são:',evenNums);
+}
+
+let array = [1, 2, 3, 4, 5, 7, 8];
+
+returnEvenValues(array);
