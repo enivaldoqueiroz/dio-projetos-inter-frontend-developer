203327ff153e43286162fb9c182921dec967cf60
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-31 18:10:43 -0300

    Variaveis var e let

---
+++ 06-VARIAVEIS-E-TIPOS-EM-JAVASCRIPT/vars/01-vars.js
@@ -0,0 +1,19 @@
+/*numberOne = 1;
+
+console.log(numberOne + 2);
+
+var numberOne;
+*/
+
+//Trabalhandd com var e 
+var firstName = "João";
+var lastName = "Souza";
+
+if(firstName === "João" ){
+    let lastName = "Rodrigues";
+    var firstName = "Pedro";
+    lastName = "Silva";
+    console.log(lastName);
+}
+
+console.log(firstName, lastName);
