7df007d5a430f6c7107e1e29b24f3e387f319372
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-02-03 22:56:37 -0300

    Solução 2 - Palindrome

--- 06-VARIAVEIS-E-TIPOS-EM-JAVASCRIPT/verificandoPalindromeJavascript.js
+++ 06-VARIAVEIS-E-TIPOS-EM-JAVASCRIPT/verificandoPalindromeJavascript.js
@@ -4,7 +4,7 @@ function verificaPalindrome(string){
 
     var mensagem = string.split("").reverse().join("") === string;
     if(mensagem === true){
-        return "Palindrome";
+        return "É palindrome";
     }
     return "Não é palindrome";
 }
@@ -14,3 +14,19 @@ function mostrarPalindrome(){
 }
 console.log(verificaPalindrome("ovo"));
 console.log(verificaPalindrome("gato"));
+
+//Solução 2
+
+function verificaPalindrome2(string){
+    if(!string) return "string inexistente";
+
+    for(let i = 0; i < string.length; i++){
+        if(string[i] !== string[string.length - 1 - i]){
+            return "É palindrome";
+        }
+        return "Não é palindrome";
+    }
+}
+
+console.log(verificaPalindrome2("ovo"));
+console.log(verificaPalindrome2("gato"));
