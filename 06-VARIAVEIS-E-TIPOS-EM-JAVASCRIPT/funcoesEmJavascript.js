7ef731760ba5315f3ca760bb2bc6c2437183a7eb
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-02-05 16:34:57 -0300

    Switch case

--- 06-VARIAVEIS-E-TIPOS-EM-JAVASCRIPT/funcoesEmJavascript.js
+++ 06-VARIAVEIS-E-TIPOS-EM-JAVASCRIPT/funcoesEmJavascript.js
@@ -122,3 +122,20 @@ function numeroPositivo(num){
 
     return true;
 }
+
+//SWITCH CASE
+
+function getAnimal(id){
+    switch (id) {
+        case 1:
+            return "cão";
+        case 2:
+            return "gato"
+        case 3:
+            return "pássaro";
+        default:
+            break;
+    }
+}
+
+getAnimal(1) // Saida cão
